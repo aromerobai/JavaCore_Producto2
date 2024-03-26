3de738a67f5f8cc61e9913d1c5256a35dfcb696f
aromerobai <aromerobai@uoc.edu>
2024-03-26 10:26:47 +0100

    first commit

---
+++ src/main/java/org/javacoreuocx/alquilatusvehiculos/AlquilatusvehiculosApplication.java
@@ -0,0 +1,12 @@
+package org.javacoreuocx.alquilatusvehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilatusvehiculosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlquilatusvehiculosApplication.class, args);
+    }
+}
